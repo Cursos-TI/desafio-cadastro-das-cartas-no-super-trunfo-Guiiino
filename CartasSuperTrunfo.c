35dcf160c8ea8f4fab5e1665f43f3c3f54c04a37
Guiiino <guialves603@gmail.com>
2025-02-08 17:37:02 +0000

    Desafio super trunfo estacio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,6 @@ int main() {
 
     char cidade[50], pais[50];
     float area, populacao, pib;
-    
 
     printf("Para começarmos o nosso jogo, vamos cadastrar nossas cartas!\n");
 
@@ -12,21 +11,21 @@ int main() {
     fgets(pais, 50, stdin);  // Lê até 49 caracteres, incluindo espaços
     printf("O Pais escolhido foi: %s\n", pais);
 
-    Printf("Perfeito! Agora informe a cidade desejada: \n");
+    printf("Perfeito! Agora informe a cidade desejada: \n");
     fgets(cidade, 50, stdin);
     printf("A cidade informada foi: %s\n", cidade);
 
     printf("Para darmos continuidade, informe a área da cidade escolhida: \n");
     scanf("%f", &area);
-    Printf("A area mencionada foi: %f\n", area);
+    printf("A area mencionada foi: %.3f\n\n", area);
 
-    Printf("Agora informe a população de sua cidade: \n");
+    printf("Agora informe a população de sua cidade: \n");
     scanf("%f", &populacao);
-    Printf("A população de sua cidade é: %f\n", populacao);
+    printf("A população de sua cidade é: %.3f\n\n", populacao);
 
     printf("Para finalizar, informe o PIB da sua cidade: \n");
-    scanf("%f", pib);
-    printf("O PIB de sua cidade é: %f\n", pib);
+    scanf("%f", &pib);
+    printf("O PIB de sua cidade é: %.3f\n\n", pib);
 
     printf("Essa foi sua carta cadastrada: \n");
 
@@ -34,12 +33,11 @@ int main() {
 
     printf("A cidade informada foi: %s\n", cidade);
 
-    Printf("A area mencionada foi: %f\n", area);
+    printf("A area mencionada foi: %.3f\n\n", area);
 
-    Printf("A população de sua cidade é: %f\n", populacao);
+    printf("A população de sua cidade é: %.3f\n\n", populacao);
 
-    printf("O PIB de sua cidade é: %f\n", pib);
-
+    printf("O PIB de sua cidade é: %.3f\n\n", pib);
 
     return 0;
 }
